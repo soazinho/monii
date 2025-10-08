96b5645d38ffb6450dc3073f48828b676d96f5d8
soazinho <hugues.carlos.soares@gmail.com>
2025-10-08 00:59:40 -0400

    remove unused code

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,11 @@
 import { createRouter } from '@tanstack/react-router'
 
-// Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
-// Create a new router instance
 export const getRouter = () => {
   return createRouter({
     routeTree,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-    context: {
-      auth: undefined!, // This will be set after we wrap the app in an AuthProvider
-    },
   })
 }
